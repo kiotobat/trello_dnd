b306ee5e9390dfa2ab5131d7720d200683afe427
kiotobat <lambar926@gmail.com>
2025-08-03 18:44:25 +0300

    six commit

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -1,6 +1,5 @@
 import globals from "globals";
 import pluginJs from "@eslint/js";
-import jest from "eslint-plugin-jest";
 
 export default [
   {
@@ -14,13 +13,5 @@ export default [
   },
   {
     ignores: ["dist/*", "coverage/*", "docs", "webpack.*.js"],
-  },
-  {
-    files: ["**/*.test.js"],
-    ...jest.configs["flat/recommended"],
-    rules: {
-      ...jest.configs["flat/recommended"].rules,
-      "jest/prefer-expect-assertions": "off",
-    },
   },
 ];
